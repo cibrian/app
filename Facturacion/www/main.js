2b1ba914d304b1983053ed84d05237082c3563cf
cibrian <rcr@MacBook-de-Ricardo.local>
2013-11-13 02:04:03 -0600

    se agregó el módulo de factura

--- Facturacion/www/main.js
+++ Facturacion/www/main.js
@@ -4,14 +4,18 @@
             var ident;
             
             function onDeviceReady(){
-            db.transaction(crearDB, errorClientes, successClientes);
-             db.transaction(muestraProductos,errorProductos);
+            db.transaction(crearDB, errorSQL, successClientes);
+            db.transaction(muestraProductos,errorSQL);
+            db.transaction(actualizarFactura, errorSQL);
+            db.transaction(muestraProductosFactura, errorSQL);
+
             }
             
             function crearDB(tx) {
             tx.executeSql('CREATE TABLE IF NOT EXISTS "Cliente" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "nombre" VARCHAR(50), "rfc" VARCHAR(50), "calle" VARCHAR(50), "numExt" VARCHAR(50), "numInt" VARCHAR(50), "colonia" VARCHAR(50), "cp" VARCHAR(50), "municipio" VARCHAR(50), "estado" VARCHAR(50), "email" VARCHAR(50))');
             tx.executeSql('CREATE TABLE IF NOT EXISTS "Producto" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "descripcion" VARCHAR(40), "unidad" VARCHAR(40), "serviciooproducto" VARCHAR(40), "costo" VARCHAR(40))');
-                  
+            tx.executeSql('CREATE TABLE IF NOT EXISTS factura (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, fecha DATETIME, regimen TEXT, emisor TEXT, formaPago TEXT, metodoPago TEXT, moneda TEXT, lugarExpedicion TEXT, subtotal DOUBLE, descuento DOUBLE, iva DOUBLE, retencionIva DOUBLE, retencionIsr DOUBLE, total DOUBLE, cbb TEXT)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS facturaDetalle (idFactura INTEGER, idProducto INTEGER, cantidad INTEGER, precioTotal DOUBLE)');
                   
             }
             
---
+++ Facturacion/www/productosFactura.js
@@ -0,0 +1,144 @@
+var itemEliminar;
+
+
+$('.remove').live("click", function() {
+                  itemEliminar =  $(this).attr('id');
+                  $('#tablaProductos').empty();
+                  //$(this).parents("tr").remove();
+                  
+                  
+                  db.transaction(eliminarProductoTabla, errorSQL);
+                  db.transaction(actualizarFactura, errorSQL);
+                  
+                  
+                  
+                  
+                  });
+
+function eliminarProductoTabla(tx) {
+    
+    
+    tx.executeSql('Delete from facturaDetalle where idProducto='+itemEliminar);
+}
+
+
+
+
+
+
+
+
+
+
+function actualizarFactura(tx) {
+    tx.executeSql('SELECT facturaDetalle.cantidad, producto.id, producto.descripcion, facturaDetalle.precioTotal from facturaDetalle INNER JOIN producto ON facturaDetalle.idProducto=producto.id where facturaDetalle.idFactura = 1', [], actualizarFacturaSql, errorSQL);
+}
+
+// Query the success callback
+//
+function actualizarFacturaSql(tx, results) {
+    var len = results.rows.length;
+    $( "#tablaProductos" ).empty();
+    
+    
+    for (var i=0; i<len; i++){
+        
+        $('#tablaProductos').append('<tr><td>'+ results.rows.item(i).cantidad +'</td><td>'+ results.rows.item(i).descripcion +'</td><td>'+ results.rows.item(i).precioTotal +'</td> <td><a id="'+results.rows.item(i).id +'" class="remove ui-btn ui-btn-up-c ui-btn-icon-notext ui-btn-corner-all ui-shadow" data-role="button" data-icon="delete" data-iconpos="notext" title="" data-theme="c"><span class="ui-btn-inner ui-btn-corner-all" aria-hidden="true"><span class="ui-btn-text"></span><span class="ui-icon ui-icon-delete ui-icon-shadow"></span></span></a></td></tr>');
+        
+        
+        
+    }
+    
+    $( "#tablaProductos" ).table("refresh" );
+}
+
+function errorSQL(err) {
+    console.log("Error processing SQL: "+err.code);
+}
+
+
+
+function muestraProductosFactura(tx){
+            tx.executeSql('SELECT * FROM producto',[],muestraProductosFacturaSql,errorSQL);
+            }
+            
+            function muestraProductosFacturaSql(tx,result){
+            $('#listaProductos2').empty();
+            $.each(result.rows,function(index){
+            var row = result.rows.item(index);
+            $('#listaProductos2').append('<li id="'+row['id']+'"><a href="#"><h3 class="ui-li-heading">'+row['descripcion']+'</h3><p class="ui-li-desc">$'+row['costo']+'</p></a></li>');
+            });
+            $( "#listaProductos2" ).listview( "refresh" );
+            
+            }
+
+$('#listaProductos2 li').live('click', function() {
+                  ident=$(this).attr("id");
+                              
+                              console.log(ident);
+                  
+                  db.transaction(prepararProducto,errorProductos);
+                  $.mobile.changePage("#formAgregarProducto", {transition : "slide"});
+                  });
+
+$('#agregarProductoFactura').live("click",function(){
+
+
+adicionProducto();
+
+
+});
+
+$('#agregarItem').live("click",function(){
+                       
+db.transaction(muestraProductosFactura, errorSQL);
+
+  $.mobile.changePage("#inventario", {transition : "slide"});
+
+
+
+
+});
+
+
+
+
+function adicionProducto(){
+                  db.transaction(adicionProductoSql, errorSQL);
+                  db.transaction(actualizarFactura, errorSQL);
+                  $.mobile.changePage("#page7", {transition : "slide"});
+                  }
+                  
+                  function adicionProductoSql(tx) {
+                  var precioVal      = $("#precioHidden").attr('value');
+                  var cantidadVal           = $("#cantidadProducto").attr('value');
+
+                  var total = precioVal * cantidadVal;
+                
+       
+            
+        tx.executeSql('INSERT INTO facturaDetalle (idFactura, idProducto, cantidad, precioTotal) VALUES (1,'+ident+','+ cantidadVal+','+ total +')');
+                  
+                      $("#cantidadProducto").val('');
+                  
+                  }
+
+function prepararProducto(tx){
+            tx.executeSql('SELECT * FROM producto WHERE id='+ident+'',[],prepararProductoSql,errorProductos);
+            }
+            
+            function prepararProductoSql(tx,result){
+            
+            $.each(result.rows,function(index){
+            var row = result.rows.item(index);
+                   
+                
+            $('#nombreProducto').val(row['descripcion']);
+            $('#precioHidden').val(row['costo']);
+           
+            });
+       
+            
+            }
+
+
